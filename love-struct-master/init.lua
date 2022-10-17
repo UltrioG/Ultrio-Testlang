7c1f15d21b6cd7d614e431afb316dc8172e3651f
JW <115539379+J-W-CDSJ6@users.noreply.github.com>
2022-10-17 17:58:59 +0800

    Added library of data structures
    Credits to https://github.com/weakman54/love-struct

---
+++ love-struct-master/init.lua
@@ -0,0 +1,22 @@
+---------------------------------------------------------------------------------------------------
+-- init.lua
+---------------------------------------------------------------------------------------------------
+-- Returns the classes in a single table
+local path = ({...})[1] .. "." 
+local struct = {}
+struct.grid = require(path .. "grid")
+struct.queue = require(path .."queue")
+struct.stack = require(path .. "stack")
+struct.tree = require(path .. "tree")
+struct.bag = require(path .. "bag")
+
+return struct
+
+---------------------------------------------------------------------------------------------------
+-- By Casey Baxter (Kadoba) Casey_Baxter@Hotmail.com, 2012
+--
+-- CC0 Public Domain Dedication 
+-- To the extent possible under law, the author(s) have dedicated all copyright and related and 
+-- neighboring rights to this software to the public domain worldwide. This software is distributed 
+-- without any warranty. 
+----------------------------------------------------------------------------------------------------	
